4c6f3144a37c486dc24c269b6f510ceb3dabda05
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-23 18:07:57 -0700

    pushing to turn in

--- index.js
+++ index.js
@@ -3,26 +3,38 @@
 // OUTPUT: the value of the 'name' attribute i.e. Benny
 // REQS: use destructuring and the function should be a single line
 
-export const getName = () => {}
+export const getName = ({ name }) => {
+  return name;
+}
 
 // INPUT: an object with a nested "address" attribute such as
 //   {name: 'Bob Smith', address: {street: 'Main Street', number: 123, city: 'Anytown', country: 'USA}}
 // OUTPUT: the string 'Bob Smith lives at 123 Main Street, Anytown, USA'
 // REQS: use destructuring and template literals
 
-export const printAddress = () => {}
+export const printAddress = ({ name, address}) => {
+  return (`${name} lives at ${address.number} ${address.street}, ${address.city}, ${address.country}`);
+}
 
 // REFACTOR CHALLENGE
 // Refactor this function so that all values in the object are destructured
-// as part of the funciton definitions (i.e. there should be no dots in the template literals)
-export const printUserInfo = (user) => {
+// as part of the function definitions (i.e. there should be no dots in the template literals)
+export const printUserInfo = ({
+  username,
+  name: { first, last },
+  info: {
+    pet: { name: petName },
+    favorites: { color, food },
+    address: { number, street, city, country },
+  },
+}) => {
   return `
-    Username: ${user.username},
-    Full Name: ${user.name.first} ${user.name.last},
-    Favorite Color: ${user.info.favorites.color},
-    Favorite Food: ${user.info.favorites.food},
-    Pet Name: ${user.info.pet.name},
-    Address: ${user.info.address.number} ${user.info.address.street}, ${user.info.address.city}, ${user.info.address.country}
+    Username: ${username},
+    Full Name: ${first} ${last},
+    Favorite Color: ${color},
+    Favorite Food: ${food},
+    Pet Name: ${petName},
+    Address: ${number} ${street}, ${city}, ${country}
     `
 }
 
@@ -31,14 +43,25 @@ export const printUserInfo = (user) => {
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {}
+export const getSum = (...rest) => {
+  let acc = 0;
+
+  for (let num of rest) {
+    acc = acc + num
+  }
+  return acc;
+}
 
 // INPUT: an unknown number of arguments
-// OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
+// OUTPUT: an array with the first two arguments 
+//destructured and the remaining in a nested array
 // REQS: use rest parameters
 // getFirstTwoArgs(1, 2, 3, 4, 5) should return [1, 2, [3, 4, 5]]
 // getFirstTwoArgs('a', 'b', 'c', 'd') should return ['a', 'b', ['c', 'd']]
-export const getFirstTwoArgs = () => {}
+export const getFirstTwoArgs = (...rest) => {
+  let arr = [...rest.slice(0, 2), rest.slice(2)]
+  return arr; 
+}
 
 // INPUT: an object with the following structure
 // {
@@ -61,16 +84,31 @@ export const getFirstTwoArgs = () => {}
 //    return a NEW object, do not modify the object passed in to the function
 //    use spread operator to create a new object
 
-export const addSneakerCount = () => {}
+export const addSneakerCount = ({ shoes, ...rest}) => {
+  const answer = {
+    ...rest, 
+    sneakerCount: shoes.length,
+  }
+  return answer;
+}
 
 // INPUT: brands from data.js
 // OUTPUT: the brand names listed
 // REQS: use Object.keys to solve
-export const getBrandNames = () => {}
+export const getBrandNames = (brands) => {
+  return Object.keys(brands)
+}
 
 // INPUT: brands from data.js
 // OUTPUT: total number of sneaker types across all brands (14)
-export const totalSneakerCount = () => {}
+export const totalSneakerCount = ({
+  Nike: { shoes: nikeShoes },
+  Puma: { shoes: pumaShoes },
+  Adidas: { shoes: adidasShoes },
+}) => {
+  let acc = 0 
+  return (acc + nikeShoes.length + pumaShoes.length + adidasShoes.length)
+}
 
 // INPUT: An object
 // OUTPUT: An array with key value pairs converted to arrays
@@ -79,6 +117,8 @@ export const totalSneakerCount = () => {}
 // convertToArray({}) => []
 // Source: https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
 
-export const convertToArray = () => {}
+export const convertToArray = (obj) => {
+  return Object.entries(obj)
+}
 
 //
